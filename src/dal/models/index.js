7511d7f85d8cd2b317a256d33aa5af4e52d73c57
Matias <acosta_matias8@hotmail.com>
2023-01-10 18:54:16 -0300

    models index

--- src/dal/models/index.js
+++ src/dal/models/index.js
@@ -10,11 +10,13 @@ const config = require(__dirname + '/../../config/index.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+/* if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log('aca')
 }
+ */
+sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
